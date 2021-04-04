d3824ab68426c4adb33164336640ee2cebb23b05
Where is Neo? <alec.bui.vhp@gmail.com>
2021-04-04 21:58:47 +0700

    (+) Layout

--- app/src/main/java/com/example/hikokainotes/NoteActivity.java
+++ app/src/main/java/com/example/hikokainotes/NoteActivity.java
@@ -7,20 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-    public static final int REQUEST_CODE_ADD_NOTE = 1;
+public class NoteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_note_activity);
 
-        ImageView addButton = findViewById(R.id.imageAddNoteMain);
-        addButton.setOnClickListener(new View.OnClickListener() {
+        ImageView backButton = findViewById(R.id.imageBack);
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivityForResult(new Intent(getApplicationContext(), NoteActivity.class), REQUEST_CODE_ADD_NOTE);
+                onBackPressed();
             }
         });
+
     }
 }
